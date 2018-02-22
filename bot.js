b009475d793782fb5a496d06b8c25698e67b09a3
Byakuya <36718024+spike12209@users.noreply.github.com>
2018-02-21 21:28:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,6 +62,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'kaneki') {
+    	message.reply('byakuya is better smh.');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
